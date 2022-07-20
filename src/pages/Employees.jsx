7d4729a7babdf6bef09e5dedec8fb5ba4f064bb4
afif <muhammad.afif5069@gmail.com>
2022-07-20 13:59:08 +0700

    initial commit

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,43 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+  Inject,
+  Page,
+  Search,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+import React from "react";
+
+import { Header } from "../components";
+import { employeesData, employeesGrid } from "../data/dummy";
+
+const Employees = () => {
+  const toolbarOptions = ["Search"];
+
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
